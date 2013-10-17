c0cb2fa70657ff539f065efcff08c46a8d83439b
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-17 19:25:30 +0530

    review

---
+++ exercise2/js/sandbox.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  
+  //Select all of the image elements on the page; log each image's alt attribute.
+  $('img').each(function() {
+    console.log($(this).attr('alt'));
+  });
+
+  //Select the search input text box, then traverse up to the form and add a class to the form. 
+  $('input[type="text"]').parent().addClass('myForm');
+
+  //Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item
+  var index = $('#myList li.current').removeClass('current').index();
+  $('#myList li').eq(index + 1).addClass('current');
+
+  //Select the select element inside #specials; traverse your way to the submit button.
+  $('#specials select[name="day"]').parent().siblings().children($('input[type="submit"]'));
+
+  //Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements. 
+  $('#slideshow li:first-child').addClass('current').siblings().addClass('disabled');
+  
+});
