afb043026ca32e495635aba16c2e643385a31ded
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-18 18:03:37 +0530

    review

---
+++ exercise4/js/inputHint.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var searchForm = $('#search');
+  var searchInput = searchForm.find($('input[type="text"]'));
+
+  //Remove the label element 
+  
+  var searchLabel = searchForm.find($('label[for="q"]')).remove().text();
+  
+  //Set the value of the search input to the text of the label element 
+  
+  searchInput.val(searchLabel)
+
+  //Add a class of "hint" to the search input 
+             
+            .addClass("hint")
+
+  //Bind a focus event to the search input that removes the hint text and the "hint" class 
+            
+            .bind('focus', function() {
+					    $(this).removeClass("hint")
+					           .val("");
+					  })
+
+  //Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
+				  
+				    .bind('blur', function() {
+				      $(this).addClass("hint")
+				             .val(searchLabel);   
+				    });
+});
