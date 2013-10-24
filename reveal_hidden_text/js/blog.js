11d818fe82a179f239ab2d0e2ab56a9153b14adf
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-24 11:43:26 +0530

    changes done

--- reveal_hidden_text/js/blog.js
+++ reveal_hidden_text/js/blog.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#blog h3').bind('click', function(e) {
+    $blogPost = $(this);
+    $blogPost.parent().siblings('li').find('p.excerpt:visible').slideUp();
+    $blogPost.siblings('p.excerpt').slideDown();
     e.preventDefault();
-    $this = $(this);
-    $this.parent().siblings().find('p.excerpt:visible').slideUp();
-    $this.siblings('p.excerpt').slideDown();
   });
 });
