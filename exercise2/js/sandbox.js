188ccac4e866bfbdc7dcb13c63c91361d2147182
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-24 10:43:52 +0530

    review

--- exercise2/js/sandbox.js
+++ exercise2/js/sandbox.js
@@ -6,14 +6,13 @@ $(document).ready(function() {
   });
 
   //Select the search input text box, then traverse up to the form and add a class to the form. 
-  $('input[type="text"]').parent().addClass('myForm');
+  $('input[name="q"]').parent().addClass('myForm');
 
   //Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item
-  var index = $('#myList li.current').removeClass('current').index();
-  $('#myList li').eq(index + 1).addClass('current');
+  var index = $('#myList li.current').removeClass('current').next().addClass('current');
 
   //Select the select element inside #specials; traverse your way to the submit button.
-  $('#specials select[name="day"]').parent().siblings().children($('input[type="submit"]'));
+  $('#specials select').parent().next().find('input[type="submit"]');
 
   //Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements. 
   $('#slideshow li:first-child').addClass('current').siblings().addClass('disabled');
