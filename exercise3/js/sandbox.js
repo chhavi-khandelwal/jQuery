5f6570ff0e639b05d1ae5b8f2fa069b1ffa22c95
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-25 11:56:38 +0530

    changes done

--- exercise3/js/sandbox.js
+++ exercise3/js/sandbox.js
@@ -3,22 +3,23 @@ $(document).ready(function() {
   //Add five new list items to the end of the unordered list #myList.
   $myList = $('#myList');
   for (var i = 0; i < 5; i++) {
-  	$myList.append($('<li>' + i + '</li>'));
+    $myList.append($('<li>' + i + '</li>'));
   }
 
   //Remove the odd list items
   $('#myList li:odd').remove();
 
+  $moduleDiv = $('div.module');
   //Add another h2 and another paragraph to the last div.module 
-  $('div.module').last().append($('<h2> Heading2 </h2>'), $('<p> Paragraph Added </p>'));
+  $moduleDiv.last().append($('<h2> Heading2 </h2>'), $('<p> Paragraph Added </p>'));
 
   //Add another option to the select element; give the option the value "Wednesday" 
   $('select[name="day"]').append($('<option>', {
-  	value: "Wednesday",
-  	text: "Wednesday"
+    value: "Wednesday",
+    text: "Wednesday"
   }));
 
   //Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
-  $('<div class="module"></div>').insertAfter($('div.module').last()).append($('img[alt="fruit"]').clone());
+  $('<div class="module"></div>').insertAfter($($moduleDiv).last()).append($('img[alt="fruit"]').clone());
 
 });
