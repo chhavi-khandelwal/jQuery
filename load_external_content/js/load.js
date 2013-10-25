08bb62aa067b3ced70a738969e86e08952fda3b9
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-25 16:03:53 +0530

    changes done

--- load_external_content/js/load.js
+++ load_external_content/js/load.js
@@ -3,21 +3,22 @@ $(document).ready(function() {
     $blog_post = $(this);
 
     //target div after the headline for each blog post 
-    var id = "target" + index;
-    var $target = $('<div id=' + id + ">" + '</div>').insertAfter($blog_post);
+    var id = "blog_load_target" + index;
+    $('<div id=' + id + '></div>').insertAfter($blog_post);
 
     // store a reference to it on the headline element
-    $blog_post.data('target_id', id);
+    $blog_post.data('blog_load_target_id', id);
 
   }).bind('click', function(event) {     //Bind a click event to load the appropriate content from blog.html into the target div  
+      
+      event.preventDefault();
 
       var $blog_post = $(this);
       var href = $blog_post.find('a').attr('href');
       var id = "#" + href.split('#')[1];
-      var $target_div = $blog_post.siblings($('#' + $blog_post.data('target_id')));
+      var $target_div = $blog_post.siblings($('#' + $blog_post.data('blog_load_target_id')));
 
       $target_div.load("blog.html " + id);
 
-      event.preventDefault();
     });
 });
