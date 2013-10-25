29fbd666e208638299760ba769d3431b41d853a9
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-25 15:07:45 +0530

    changes done

--- exercise5/js/tabs.js
+++ exercise5/js/tabs.js
@@ -1,27 +1,30 @@
 $(document).ready(function() {
-	var moduleDivs = $('div.module');
-	//Hide all of the modules. 
+  var moduleDivs = $('div.module');
+  //Hide all of the modules. 
   moduleDivs.hide();
 
   var firstModule = moduleDivs.first();
-  //Create an unordered list element before the first module. 
-  $('<ul></ul>').insertBefore(firstModule);
+  //Create an unordered list element before the first module.
+  var ul = $('<ul>');
+  ul.insertBefore(firstModule);
   
   var unorderedList = firstModule.prev();
-  //use the text of the h2 element as the text for a list item that you add to the unordered list element. 
+  
+  //use the text of the h2 element as the text for a list item that you add to the unordered list element.
   moduleDivs.each(function() {
-  	var $this = $(this);
-  	var listItem = $('<li>' + $(this).find('h2').text() + '</li>');
-  	listItem.appendTo(unorderedList);
+    var $this = $(this);
+    var listItem = $('<li>');
+    listItem.html($this.find('h2').text());
+    listItem.appendTo(unorderedList);
 
   //Bind a click event to the list item
-  	listItem.bind('click', function() {
+    listItem.bind('click', function() {
       listItem.addClass('current').siblings().removeClass('current');
       $this.show().siblings('.module').hide();
-  	});
+    });
   });
   
   //show the first tab. 
-  unorderedList.children().eq(0).addClass('current').siblings().removeClass('current');
-  moduleDivs.eq(0).show() 
+  unorderedList.children().first().addClass('current').siblings().removeClass('current');
+  moduleDivs.first().show();
 });
