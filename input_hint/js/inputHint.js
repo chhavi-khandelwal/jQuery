112a7636dac86383518ed3e484c6367c36c13fea
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-25 16:42:06 +0530

    changes done

--- input_hint/js/inputHint.js
+++ input_hint/js/inputHint.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
   var searchForm = $('#search');
-  var searchInput = searchForm.find($('input[type="text"]'));
+  var searchInput = searchForm.find($('input[name="q"]'));
 
   //Remove the label element 
   
   var searchLabel = searchForm.find($('label[for="q"]')).remove().text();
-  
-  searchInput.val(searchLabel)  //Set the value of the search input to the text of the label element 
-    
-    .addClass("hint")  //Add a class of "hint" to the search input 
 
-    .bind('focus', function() {   //Bind a focus event to the search input that removes the hint text and the "hint" class
-      $(this).removeClass("hint")
+  /*1.Set the value of the search input to the text of the label element 
+  2.Add a class of "hint" to the search input 
+  3.Bind a focus event to the search input that removes the hint text and the "hint" class
+  4.Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered*/
+  searchInput.val(searchLabel)
+    .addClass("hint")
+    .bind('focus', function() {
+      $(this).removeClass("hint");
       if ($(this).val() == searchLabel)
         $(this).val("");
-    }) 
-
-    .bind('blur', function() {    //Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
-      $(this).addClass("hint")
-      if ($(this).val() == "")
-        $(this).val(searchLabel);   
+    })
+    .bind('blur', function() {
+      $(this).addClass("hint");
+      if (!$(this).val().trim())
+        $(this).val(searchLabel);
     });
 });
