214bb754769fba050e4b1d2ca530ff27425340b7
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-25 16:56:23 +0530

    explanation added

--- exercise7/js/navigation.js
+++ exercise7/js/navigation.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
   $('#nav li').hover(
     function() {
-      var $this = $(this);
-      $this.addClass('hover');
-      $this.children('ul').slideDown();
-    },
-    function() {
-      var $this = $(this);
-      $this.removeClass('hover');
-      $this.children('ul:visible').slideUp();
+      $this = $(this);
+      $this.toggleClass('hover');
+      $this.children('ul').slideToggle();
   });
 });
