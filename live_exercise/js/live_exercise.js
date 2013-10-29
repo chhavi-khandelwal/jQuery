7d5a31a7940d5fc9a648bd23bc03858658dbc0c9
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-29 10:50:19 +0530

    changes done

--- live_exercise/js/live_exercise.js
+++ live_exercise/js/live_exercise.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
   var $mainContainer = $('#main-container');
-  
+  var i = 0;
+
   // add a div to the stack with an incremental number on button click
   $('#add-button').bind('click', function() {
-    var divCount = $mainContainer.find('div').length;
-    $('<div>' + (divCount + 1) + '</div>').addClass('inner-container').prependTo($mainContainer);
+    i++;
+    $('<div class="inner-div">' + i + '</div>').addClass('inner-container').prependTo($mainContainer);
   });
   
   //clicking any item in the stack should highlight that item
-  $mainContainer.delegate('div:not(div:first)', 'click', function() {
+  $mainContainer.delegate('div.inner-div:not(div.inner-div:first)', 'click', function() {
     $(this).addClass('highlight');
   });
   
   // clicking the last item on the stack should remove that item from the stack
-  $mainContainer.on('click', 'div:first-child', function() {
+  $mainContainer.on('click', 'div.inner-div:first-child', function() {
     $(this).remove();
+    i--;
   });
 });
