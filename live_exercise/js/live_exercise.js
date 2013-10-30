29cf232041caeec9679a8a86f6d060150a02386d
vinsol <vinsol@sahil-HP-430-Notebook-PC.(none)>
2013-10-30 11:27:59 +0530

    on replaced, variable name changed

--- live_exercise/js/live_exercise.js
+++ live_exercise/js/live_exercise.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   var $mainContainer = $('#main-container');
-  var i = 0;
+  var innerDivCount = 0;
 
   // add a div to the stack with an incremental number on button click
   $('#add-button').bind('click', function() {
-    i++;
-    $('<div class="inner-div">' + i + '</div>').addClass('inner-container').prependTo($mainContainer);
+    innerDivCount++;
+    $('<div class="innerDiv">' + innerDivCount + '</div>').addClass('inner-container').prependTo($mainContainer);
   });
   
   //clicking any item in the stack should highlight that item
-  $mainContainer.delegate('div.inner-div:not(div.inner-div:first)', 'click', function() {
+  $mainContainer.delegate('div.innerDiv:not(div.innerDiv:first)', 'click', function() {
     $(this).addClass('highlight');
   });
   
   // clicking the last item on the stack should remove that item from the stack
-  $mainContainer.on('click', 'div.inner-div:first-child', function() {
+  $mainContainer.delegate('div.innerDiv:first-child', 'click', function() {
     $(this).remove();
-    i--;
+    innerDivCount--;
   });
 });
