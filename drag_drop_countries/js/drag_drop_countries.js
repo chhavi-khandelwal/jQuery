28031fa7a6d9e503e19f225879f2a50174c2459f
Chhavi <chhavi@vinsol.com>
2013-11-16 14:57:04 +0530

    class given to ul, only draggable element can be dropped, removed removeCountry()

--- drag_drop_countries/js/drag_drop_countries.js
+++ drag_drop_countries/js/drag_drop_countries.js
@@ -3,77 +3,44 @@ $(document).ready(function() {
 });
 
 function init() {
-  var listTransfer = new ListTransfer(),
-    $addlist = $('#addlist');
+  var listTransfer = new ListTransfer();
+}
 
-//drops country when dragged over #removelist ul
-  $('#removelist').droppable({
-    drop: function(event, ui) {
-      if ($(ui.draggable).hasClass('dragAndDrop') && $(ui.draggable).hasClass('draggedFromaddlist')) {
-        $(ui.draggable).removeClass('draggedFromaddlist');
-        transferredCountry = listTransfer.makeListItem(ui, this);
-        listTransfer.dragCountry(transferredCountry);
-        listTransfer.removeCountry(ui, $('#addlist li'));
+function ListTransfer() {
+  
+  //drags selected country
+  this.dragCountry = function(country) {
+    country.draggable({
+      helper : 'clone',
+      revert : 'invalid',
+      drag: function(event, ui) {
+        $('.countrylist').addClass('draggedTo');
+        var listCountry = event.target;
+        dropCountry(listCountry);
       }
-    } 
-  });
-
-  //drops country when dragged over #addlist ul
-  $addlist.droppable({
-    drop: function(event, ui) {
-      if ($(ui.draggable).hasClass('dragAndDrop') && $(ui.draggable).hasClass('draggedFromremovelist')) {
-        $(ui.draggable).removeClass('draggedFromremovelist');
-        var transferredCountry = listTransfer.makeListItem(ui, this);
-        listTransfer.dragCountry(transferredCountry);
-        listTransfer.removeCountry(ui, $('#removelist li'));
+    });
+   } 
+  function dropCountry(listCountry) {
+    $('.draggedTo').droppable({
+      drop: function() {
+        $(listCountry).closest('ul').removeClass('draggedTo');
+        if ($(this).hasClass('draggedTo')) {
+          $(this).append(listCountry);
+        }
       }
-    } 
-  });
+    });
+  }
+  this.dragCountry($('.dragAndDrop'));
 
   //drags country which cannot be dropped
   $('.doNotDrop').draggable({
     helper: "clone",
     revert: true,
-    start: function() {
-      $addlist.addClass('onlyDrag');
+    start: function(event) {
+      $(event.target).closest('ul').addClass('onlyDrag');
     },
-    stop: function() {
-      $addlist.removeClass('onlyDrag');
+    stop: function(event) {
+      $(event.target).closest('ul').removeClass('onlyDrag');
     }
   });
-}
-
-function ListTransfer() {
-  var listTransfer = this,
-    transferredCountry = null;
-  
-  //drags selected country
-  this.dragCountry = function(country) {
-    country.draggable({
-      helper: "clone",
-      revert: false,
-      start: function() {
-        var $country = $(this);
-        $country.addClass('draggedFrom' + $country.parent().attr('id'));
-      }
-    });
-  }
-  this.dragCountry($('.dragAndDrop'));
-  
-  
-
-  //removes country from list once dropped to other list 
-  this.removeCountry = function(ui, $list) {
-    $list.each(function() {
-      if(ui.helper.text() == $(this).text()) {
-        $(this).remove();
-      }
-    });
-  }
-  
-  //creates new list item
-  this.makeListItem = function(ui, self) {
-    var country = $("<li></li>").text(ui.draggable.text()).addClass('dragAndDrop').appendTo(self);
-    return country;
-  }
 }
